decb021c4345d5f8e55387d93a75faf65610e250
Jon Warbrick <jw35@cam.ac.uk>
2019-06-04 13:40:23 +0100

    Schedule runs on 5-sec boundary; reformat output

--- api_v1_decoder.py
+++ api_v1_decoder.py
@@ -7,7 +7,16 @@ import time
 SOURCE = 'https://api.vivacitylabs.com/cambridge-mill-road/v1/counts'
 req_params = {'apikey': 'deSwbTn3duihqRLyvgfFGdvft2w28nhKthPWa'}
 
+# Top of the next minute
+next = datetime.datetime.now().replace(second=0, microsecond=0) + datetime.timedelta(minutes=1)
+#print(F'Start at {next}')
+
 for x in range(100):
+
+    # Sleep to our next run time
+    delay = (next - datetime.datetime.now()).total_seconds()
+    #print(F'Sleeping for {delay}')
+    time.sleep(delay)
 
     now = datetime.datetime.now()
     r = requests.get(SOURCE, req_params)
@@ -20,11 +29,14 @@ for x in range(100):
 
     countline = device['countlines'][0]
 
+    id = countline["countlineId"]
+
     f = datetime.datetime.fromtimestamp(countline["fromTime"]/1000)
     t = datetime.datetime.fromtimestamp(countline["toTime"]/1000)
 
     d = t-f
 
-    print(F'{now}: from {f} to {t} ({d})')
+    print(F'{id} - {now}: from={f:%H:%M:%S.%f} to={t:%H:%M:%S.%f} {d}', flush=True)
 
-    time.sleep(5)
+    # Schedule the next run in 5 seconds
+    next = next + datetime.timedelta(seconds=5)
