7ba780d452282fce8257646e881406dcb077ffdf
Jon Warbrick <jw35@cam.ac.uk>
2019-07-11 16:48:19 +0100

    Largely working graphit.py
    The version created the graphs sent to Dan Clarke, etc. (but with
    fixed labelling of Station Road).

--- vivacity/graphit.py
+++ vivacity/graphit.py
@@ -19,26 +19,26 @@ A3L = (420*MM_TO_INCH, 297*MM_TO_INCH)
 
 ROWS_PER_PAGE = 6
 START = date(2019, 5, 10)
-END = date(2019, 7, 8)
+END = date(2019, 7, 10)
 YMAX = 13000
 FIGSIZE = A3L
 
 COUNTLINES = {
-    '13069': {'name': 'Tennison Road', 'in': 'north-bound', 'out': 'south-bound'},
-    '13070': {'name': 'Coleridge Road', 'in': 'south-bound', 'out': 'north-bound'},
-    '13071': {'name': 'Mill Road (Prene Road end)', 'in': 'west-bound', 'out': 'east-bound'},
-    '13072': {'name': 'Vinery Road', 'in': 'south-bound', 'out': 'north-bound'},
-    '13073': {'name': 'Cherry Hinton Road', 'in': 'east-bound', 'out': 'west-bound'},
-    '13074': {'name': 'Station Road', 'in': 'west-bound', 'out': 'east-bound'},
-    '13075': {'name': 'East Road', 'in': 'north-east-bound', 'out': 'south-west-bound'},
-    '13076': {'name': 'Coldhams Lane', 'in': 'south-bound', 'out': 'north-bound'},
-    '13077': {'name': 'Mill Road (city end)', 'in': 'south-east-bound', 'out': 'north-west-bound'},
-    '13078': {'name': 'Carter Bridge', 'in': 'west-bound', 'out': 'east-bound'},
-    '13079': {'name': 'Milton Road', 'in': 'north-east-bound', 'out': 'south-west-bound'},
-    '13080': {'name': 'Hills Road', 'in': 'south-bound', 'out': 'north-bound'},
-    '13081': {'name': 'Newmarket Road', 'in': 'west-bound', 'out': 'east-bound'},
-    '13082': {'name': 'Histon Road', 'in': 'south-bound', 'out': 'north-bound'},
-    '13086': {'name': 'Perne Road', 'in': 'north-bound', 'out': 'south-bound'},
+    '13069': {'name': 'Tennison Road', 'in': 'N-bound', 'out': 'S-bound'},
+    '13070': {'name': 'Coleridge Road', 'in': 'S-bound', 'out': 'N-bound'},
+    '13071': {'name': 'Mill Road (E end)', 'in': 'E-bound', 'out': 'W-bound'},
+    '13072': {'name': 'Vinery Road', 'in': 'S-bound', 'out': 'N-bound'},
+    '13073': {'name': 'Cherry Hinton Road', 'in': 'E-bound', 'out': 'W-bound'},
+    '13074': {'name': 'Station Road', 'in': 'W-bound', 'out': 'E-bound'},
+    '13075': {'name': 'East Road', 'in': 'NE-bound', 'out': 'SW-bound'},
+    '13076': {'name': 'Coldhams Lane', 'in': 'S-bound', 'out': 'N-bound'},
+    '13077': {'name': 'Mill Road (NW end)', 'in': 'SE-bound', 'out': 'NW-bound'},
+    '13078': {'name': 'Carter Bridge', 'in': 'W-bound', 'out': 'E-bound'},
+    '13079': {'name': 'Milton Road', 'in': 'NE-bound', 'out': 'SW-bound'},
+    '13080': {'name': 'Hills Road', 'in': 'S-bound', 'out': 'N-bound'},
+    '13081': {'name': 'Newmarket Road', 'in': 'W-bound', 'out': 'E-bound'},
+    '13082': {'name': 'Histon Road', 'in': 'S-bound', 'out': 'N-bound'},
+    '13086': {'name': 'Perne Road', 'in': 'N-bound', 'out': 'S-bound'},
     }
 
 
@@ -110,20 +110,39 @@ def do_graph(df, ax, col):
 
     # df.plot.bar(y=[col], ax=ax, ylim=(0, YMAX), legend=False)
 
-    ax.bar(df.index, df[col], zorder =3)
+    ax.bar(df.index, df[col], zorder =3, align='edge')
+
+    ax.set_ylim([0, YMAX])
+    ax.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(base=2500))
+    ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(base=500))
 
     ax.grid(axis='y', zorder=2)
     ax.xaxis.set_major_locator(matplotlib.dates.WeekdayLocator(byweekday=matplotlib.dates.MO))
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%d\n%b'))
     ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator())
 
+    # "Mill Road bridge will be closed to motor traffic for eight weeks from Monday 1 July"
+    #
+    # "The bridge will be fully closed from 8.45am until 8.45pm on
+    # "Friday 5 July to Monday 8 July; Thursday 11 July to Saturday 13
+    # "July; Sunday 28 July to Wednesday 31 July and Saturday 3 August
+    # "to Monday 5 August.
+    #
+    # https://www.cambridgenetwork.co.uk/news/mill-road-remain-open-while-bridge-closed
+
+    # All end dates extended by one day to include the last day
+
     left, right = ax.get_xlim()
-    ax.axvspan(date(2019, 7, 1), matplotlib.dates.num2date(right), facecolor='k', alpha=0.1, zorder=1)
-    # Reset xlim becasue otherwise the axvspan makes them bigger each time around!
+    ax.axvspan(date(2019, 7, 1), date(2019, 8, 24), facecolor='k', alpha=0.1, zorder=1)
+    ax.axvspan(date(2019, 7, 5), date(2019, 7, 9), facecolor='k', alpha=0.05, zorder=1)
+    ax.axvspan(date(2019, 7, 11), date(2019, 7, 14), facecolor='k', alpha=0.05, zorder=1)
+    ax.axvspan(date(2019, 7, 28), date(2019, 8, 1), facecolor='k', alpha=0.05, zorder=1)
+    ax.axvspan(date(2019, 8, 3), date(2019, 8, 6), facecolor='k', alpha=0.05, zorder=1)
+    # Reset xlim because otherwise the axvspan makes them bigger each time around!
     ax.set_xlim(left, right)
 
 
-def do_graphs(axs, row, countline, direction, start, end):
+def do_graphs(axs_row, countline, direction, start, end):
 
     df = pd.DataFrame(get_data(countline, direction, start, end))
     df.columns = ('Date', 'Pedestrian', 'Cyclist', 'Motor')
@@ -131,33 +150,29 @@ def do_graphs(axs, row, countline, direction, start, end):
 
     df = df.resample('D').sum()
 
-    do_graph(df, axs[row, 0], col='Pedestrian')
-    do_graph(df, axs[row, 1], col='Cyclist')
-    do_graph(df, axs[row, 2], col='Motor')
+    do_graph(df, axs_row[0], col='Pedestrian')
+    do_graph(df, axs_row[1], col='Cyclist')
+    do_graph(df, axs_row[2], col='Motor')
 
-    title = (f"{COUNTLINES[countline]['name']} "
-             f"{COUNTLINES[countline][direction]} "
-             f"[{countline} {direction}]")
-    axs[row, 0].annotate(title, xy=(0.01, 0.85), xycoords="axes fraction")
-    axs[row, 0].set(xlabel='', ylabel='Per day')
-
-    if row % ROWS_PER_PAGE == 0:
-        axs[row, 0].set_title('Pedestrian')
-        axs[row, 1].set_title('Cyclist')
-        axs[row, 2].set_title('Motor vehicle')
+    title = (f"{COUNTLINES[countline]['name']}\n"
+             f"{COUNTLINES[countline][direction]}\n"
+             "Per day")
+    axs_row[0].set(xlabel='', ylabel=title)
 
 
 def setup_figure():
 
-    fig, axs = pyplot.subplots(nrows=ROWS_PER_PAGE,
-                               ncols=3,
-                               sharex=True,
-                               sharey=True,
-                               figsize=FIGSIZE)
+    fig, axs_list = pyplot.subplots(nrows=ROWS_PER_PAGE,
+                                    ncols=3,
+                                    sharex=True,
+                                    sharey=True,
+                                    figsize=FIGSIZE)
 
-    #fig.tight_layout()
+    axs_list[0, 0].set_title('Pedestrian')
+    axs_list[0, 1].set_title('Cyclist')
+    axs_list[0, 2].set_title('Motor vehicle')
 
-    return fig, axs
+    return fig, axs_list
 
 
 def run():
@@ -187,11 +202,13 @@ def run():
             for direction in ['in', 'out']:
                 if row % ROWS_PER_PAGE == 0:
                     if row > 0:
+                        fig.tight_layout()
                         pdf.savefig(fig)
-                    fig, axs = setup_figure()
-                do_graphs(axs, row % ROWS_PER_PAGE, countline, direction, START, END)
+                    fig, axs_list = setup_figure()
+                do_graphs(axs_list[row % ROWS_PER_PAGE], countline, direction, START, END)
                 row += 1
 
+        fig.tight_layout()
         pdf.savefig(fig)
 
 
