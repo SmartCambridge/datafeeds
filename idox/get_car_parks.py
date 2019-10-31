a1ba9924e124b3984029a539cb771d2354e02e48
Jon Warbrick <jw35@cam.ac.uk>
2019-10-31 13:20:52 +0000

    Tweak

--- idox/get_car_parks.py
+++ idox/get_car_parks.py
@@ -28,13 +28,21 @@ def run():
     for situation in tree.findall('.//d2:situation', ns):
         situation_record = situation.find('d2:situationRecord', ns)
 
-        name = situation_record.findtext('d2:nonGeneralPublicComment/d2:comment/d2:value', '', ns)
         id = situation_record.findtext('d2:carParkIdentity', '', ns)
+        if id != 'CAMB-CP001':
+            continue
+
+        timestamp = situation_record.findtext('d2:situationRecordCreationTime', '', ns)
+
+        name = situation_record.findtext('d2:nonGeneralPublicComment/d2:comment/d2:value', '', ns)
         occupancy = int(situation_record.findtext('d2:carParkOccupancy', '', ns))
         capacity = int(situation_record.findtext('d2:totalCapacity', '', ns))
+        fill_rate = int(situation_record.findtext('d2:fillRate', '', ns))
+        exit_rate = int(situation_record.findtext('d2:exitRate', '', ns))
 
-        results.append({'id': id, 'name': name, 'capacity': capacity,
-            'occupancy': occupancy, 'free': capacity-occupancy})
+        results.append({'time': timestamp, 'id': id, 'name': name, 'capacity': capacity,
+            'occupancy': occupancy, 'free': capacity-occupancy, 'fill_rate': fill_rate,
+            'exit_rate': exit_rate})
 
     print(tabulate(results, headers='keys'))
 
